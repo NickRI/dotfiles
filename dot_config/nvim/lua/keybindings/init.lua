13a4aceff956ba978137cb0092f00d903e0b8773
NickRI <nicktt2008@yandex.ru>
2023-09-02 18:37:51 +0200

    Update .config/nvim/lua/keybindings/init.lua Update .config/nvim/lua/plugins/init.lua Update .config/nvim/lua/plugins/telescope.lua

--- dot_config/nvim/lua/keybindings/init.lua
+++ dot_config/nvim/lua/keybindings/init.lua
@@ -12,5 +12,3 @@ map('n', '<c-j>', '<c-w>j', opts)
 map('n', '<c-h>', '<c-w>h', opts)
 map('n', '<c-k>', '<c-w>k', opts)
 map('n', '<c-l>', '<c-w>l', opts)
-
-
--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -1,3 +1,83 @@
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+
+require("lazy").setup({
+    'EdenEast/nightfox.nvim',
+    'chriskempson/base16-vim',
+    'nvim-treesitter/nvim-treesitter',
+
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope-project.nvim',
+            'nvim-telescope/telescope-file-browser.nvim',
+            'nvim-telescope/telescope-github.nvim',
+        }
+    },
+
+    {
+        'pwntester/octo.nvim',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'nvim-telescope/telescope.nvim',
+            'kyazdani42/nvim-web-devicons',
+        }
+    },
+    
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'kyazdani42/nvim-web-devicons', opt = true }
+    },
+
+    {
+        'SmiteshP/nvim-gps',
+        dependencies = { 'nvim-treesitter/nvim-treesitter' },
+        config = function()
+            require("nvim-gps").setup()
+        end,
+    },
+    
+    'neovim/nvim-lspconfig',
+    
+    {
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-buffer',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-cmdline',
+            'L3MON4D3/LuaSnip',
+            'saadparwaiz1/cmp_luasnip'
+        }
+    },
+    
+    'lewis6991/gitsigns.nvim',
+    'lukas-reineke/lsp-format.nvim',
+    'akinsho/toggleterm.nvim',
+    {
+        'folke/noice.nvim',
+        dependencies = {
+            'MunifTanjim/nui.nvim',
+            'rcarriga/nvim-notify'
+        }
+    },
+})
+
+
 require('plugins.tree-sitter')
 require('plugins.telescope')
 require('plugins.octo')
@@ -6,86 +86,4 @@ require('plugins.lspconfig')
 require('plugins.cmp')
 require('plugins.gitsigns')
 require('plugins.toggleterm')
---require('plugins.noice')
-
-
-return require('packer').startup(function()
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
-
-    use 'EdenEast/nightfox.nvim'
-
-	use 'chriskempson/base16-vim'
-    
-	use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
-    }
-
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.0',
-        requires = { 
-            {'nvim-lua/plenary.nvim'},
-            {'nvim-telescope/telescope-project.nvim'},
-            {'nvim-telescope/telescope-file-browser.nvim'},
-            {'nvim-telescope/telescope-github.nvim'},
-        }
-    }
-
-    use {
-        'pwntester/octo.nvim',
-        requires = {
-            'nvim-lua/plenary.nvim',
-            'nvim-telescope/telescope.nvim',
-            'kyazdani42/nvim-web-devicons',
-        }
-    }
-
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-    }
-
-    use {
-        'SmiteshP/nvim-gps',
-        requires = { 'nvim-treesitter/nvim-treesitter' },
-        config = function()
-            require("nvim-gps").setup()
-        end,
-    }
-
-    use 'neovim/nvim-lspconfig'
-
-    use {
-        'hrsh7th/nvim-cmp',
-        requires = {
-            'hrsh7th/cmp-nvim-lsp',
-            'hrsh7th/cmp-buffer',
-            'hrsh7th/cmp-path',
-            'hrsh7th/cmp-cmdline',
-            'L3MON4D3/LuaSnip',
-            'saadparwaiz1/cmp_luasnip'
-        }
-    }
-
-    use {
-        'lewis6991/gitsigns.nvim',
-        -- tag = 'release' -- To use the latest release
-    }
-
-	use {
-		'lukas-reineke/lsp-format.nvim'
-	}
-
-	use {
-		'akinsho/toggleterm.nvim'
-	}
-
-	use {
-		'folke/noice.nvim',
-		requires = {
-			'MunifTanjim/nui.nvim',
-			'rcarriga/nvim-notify'
-		}
-	}
-end)
+require('plugins.noice')
