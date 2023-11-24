af257c9541001f02f4494cf81d68030a28ab9dc7
NickRI <nicktt2008@yandex.ru>
2023-11-24 16:07:18 +0100

    Simplify and extend mimes

--- home/modules/mime.nix
+++ home/modules/mime.nix
@@ -1,22 +1,19 @@
 {config, ...}:
-let apps = {
-  "image/jpeg" = "eog.desktop";
-  "image/gif" = "eog.desktop";
-  "image/png" = "eog.desktop";
-  "image/tiff" = "eog.desktop";
-  "image/webp" = "eog.desktop";
-  "image/svg+xml" = [ "com.boxy_svg.BoxySVG.desktop" ];
-  "application/pdf" = [ "org.gnome.Evince.desktop" "xournalpp.desktop" ];
-  "text/plain" = "org.gnome.TextEditor.desktop";
-}; in {
+
+{
   config = {
     xdg.mimeApps = {
       enable = true;
-      associations.added = apps;
-      defaultApplications = apps;
-      associations.removed = {
-          "image/png" = [ "chromium-browser.desktop" "com.boxy_svg.BoxySVG.desktop" ];
-          "application/pdf" = [ "chromium-browser.desktop" "com.boxy_svg.BoxySVG.desktop" ];
+      defaultApplications = {
+        "image/jpeg" = "org.gnome.eog.desktop";
+        "image/gif" = "org.gnome.eog.desktop";
+        "image/png" = "org.gnome.eog.desktop";
+        "image/tiff" = "org.gnome.eog.desktop";
+        "image/webp" = "org.gnome.eog.desktop";
+        "image/svg+xml" = [ "com.boxy_svg.BoxySVG.desktop" ];
+        "application/pdf" = [ "org.gnome.Evince.desktop" "com.github.xournalpp.xournalpp.desktop" ];
+        "text/plain" = "org.gnome.TextEditor.desktop";
+        "application/vnd.sqlite3" = "org.nickvision.money.desktop";
       };
     };
   };
