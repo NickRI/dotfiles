d2fd98b3d77dfbd277151b8e9deb19b519b81d12
NickRI <nicktt2008@yandex.ru>
2023-12-03 00:52:58 +0100

    Migrate for Loupe

--- home/modules/mime.nix
+++ home/modules/mime.nix
@@ -5,11 +5,11 @@
     xdg.mimeApps = {
       enable = true;
       defaultApplications = {
-        "image/jpeg" = "org.gnome.eog.desktop";
-        "image/gif" = "org.gnome.eog.desktop";
-        "image/png" = "org.gnome.eog.desktop";
-        "image/tiff" = "org.gnome.eog.desktop";
-        "image/webp" = "org.gnome.eog.desktop";
+        "image/jpeg" = "org.gnome.Loupe.desktop";
+        "image/gif" = "org.gnome.Loupe.desktop";
+        "image/png" = "org.gnome.Loupe.desktop";
+        "image/tiff" = "org.gnome.Loupe.desktop";
+        "image/webp" = "org.gnome.Loupe.desktop";
         "image/svg+xml" = [ "com.boxy_svg.BoxySVG.desktop" ];
         "application/pdf" = [ "org.gnome.Evince.desktop" "com.github.xournalpp.xournalpp.desktop" ];
         "text/plain" = "org.gnome.TextEditor.desktop";
