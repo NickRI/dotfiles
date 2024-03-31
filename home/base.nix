22fde92a1edc3888cac6024c325f132d3cc63faa
NickRI <nicktt2008@yandex.ru>
2024-04-01 03:42:41 +0600

    Add ffmpeg packages

--- home/base.nix
+++ home/base.nix
@@ -68,6 +68,8 @@
     tradingview
 
     nvd
+
+    ffmpeg
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
