dd22b4a5e41cc5b3e55231fcc8c1c6f80c618e8c
NickRI <nicktt2008@yandex.ru>
2024-09-07 16:14:16 +0200

    Fixing versions of installed go cli apps

--- home/modules/golang/default.nix
+++ home/modules/golang/default.nix
@@ -1,35 +1,38 @@
 { config, unstable, pkgs, ... }:
 let
     inherit (unstable) fetchFromGitHub;
-    #TODO: Wait until vendorHash will be able to change
-    #TODO: https://nixpkgs-tracker.ocfox.me/?pr=225051
-#    minimock = unstable.go-minimock.overrideAttrs(oldAttrs: rec {
-#        version = "3.1.3";
-##        vendorHash = "";
-#
-#        src = fetchFromGitHub {
-#          owner = oldAttrs.src.owner;
-#          repo = oldAttrs.src.repo;
-#          rev = "v${version}";
-#          sha256 = "sha256-6n5FOHTfsLYqnhlDO3etMnrypeOElmwdvoFQb3aSBts=";
-#        };
-#    });
 
-    go-task = unstable.go-task.overrideAttrs(oldAttrs: rec {
-        version = "3.38.0";
-        vendorHash = "";
+    sqlc = unstable.sqlc.overrideAttrs(oldAttrs: rec {
+        version = "1.27.0";
+        vendorHash = "sha256-ndOw3uShF5TngpxYNumoK3H3R9v4crfi5V3ZCoSqW90=";
 
         src = fetchFromGitHub {
-          owner = oldAttrs.pname;
+          owner = oldAttrs.src.owner;
           repo = oldAttrs.src.repo;
-          rev = "refs/tags/v${version}";
-          hash = "sha256-mz/07DONaO3kCxOXEnvWglY0b9JXxCXjTrVIEbsbl98=";
+          rev = "v${version}";
+          sha256 = "sha256-wxQ+YPsDX0Z6B8whlQ/IaT2dRqapPL8kOuFEc6As1rU=";
         };
+    });
+
+    minimock = unstable.go-minimock.overrideAttrs(oldAttrs: rec {
+        version = "3.1.3";
+        vendorHash = "sha256-fiSU2NB9rWIPQLdnui5CB5VcadTVUg2JaO3ma7DAYqo=";
+
+        src = fetchFromGitHub {
+          owner = oldAttrs.src.owner;
+          repo = oldAttrs.src.repo;
+          rev = "v${version}";
+          sha256 = "sha256-6n5FOHTfsLYqnhlDO3etMnrypeOElmwdvoFQb3aSBts=";
+        };
+
+        ldflags = [
+          "-s" "-w" "-X main.version=${version}"
+        ];
     });
 
     enumer = unstable.enumer.overrideAttrs(oldAttrs: rec {
         version = "1.5.10";
-        vendorHash = "";
+        vendorHash = "sha256-CJCay24FlzDmLjfZ1VBxih0f+bgBNu+Xn57QgWT13TA=";
 
         src = fetchFromGitHub {
           owner = oldAttrs.src.owner;
@@ -40,46 +43,16 @@ let
     });
 
     golangci-lint = unstable.golangci-lint.overrideAttrs(oldAttrs: rec {
-        version = "1.59.1";
-        vendorHash = "";
+        version = "1.60.3";
+        vendorHash = "sha256-ixeswsfx36D0Tg103swbBD8UXXLNYbxSMYDE+JOm+uw=";
 
         src = fetchFromGitHub {
           owner = oldAttrs.src.owner;
           repo = oldAttrs.src.repo;
           rev = "refs/tags/v${version}";
-          hash = "sha256-VFU/qGyKBMYr0wtHXyaMjS5fXKAHWe99wDZuSyH8opg=";
+          hash = "sha256-0ScdJ5td2N8WF1dwHQ3dBSjyr1kqgrzCfBzbRg9cRrw=";
         };
     });
-
-
-    scc = unstable.scc.overrideAttrs(oldAttrs: rec {
-        version = "3.3.5";
-        vendorHash = "";
-
-        src = fetchFromGitHub {
-          owner = oldAttrs.src.owner;
-          repo = oldAttrs.src.repo;
-          rev = "refs/tags/v${version}";
-          hash = "sha256-7qenc/1FEwiyR7qz6u8L35Wb8zAUVQ5sG5bvYpZKdzs=";
-        };
-    });
-
-
-
-    goose = unstable.goose.overrideAttrs(oldAttrs: rec {
-        version = "3.21.1";
-
-        checkFlags = [];
-
-        src = fetchFromGitHub {
-          owner = oldAttrs.src.owner;
-          repo = oldAttrs.src.repo;
-          rev = "refs/tags/v${version}";
-          hash = "sha256-Klmgw5dYt2/JYc0nuqIZwos3/onlRwsfzTOJ/Yi2pMw=";
-        };
-    });
-
-
 in
 {
   imports = [
@@ -97,20 +70,20 @@ in
     };
 
     home.packages = [
-      unstable.sqlc
       unstable.cfssl
       unstable.protoc-gen-go
       unstable.protoc-gen-go-grpc
+      unstable.scc
       unstable.glow
       unstable.soft-serve
-      unstable.golangci-lint
       unstable.lazydocker
       unstable.delve
-#      minimock
-      go-task
+      unstable.goose
+      unstable.go-task
+      golangci-lint
+      minimock
       enumer
-      scc
-      goose
+      sqlc
     ];
 
     home.sessionPath = ["$HOME/go/bin"];
