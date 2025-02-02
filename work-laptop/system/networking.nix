1bd1e5e5c200f9c1beae1f7e87582742b2f60729
NickRI <nicktt2008@yandex.ru>
2025-02-02 03:58:12 +0100

    Sops for home wifi connection

--- work-laptop/system/networking.nix
+++ work-laptop/system/networking.nix
@@ -5,12 +5,43 @@
     networking.hostName = "work-laptop"; # Define your hostname.
     # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+    sops = {
+      secrets = {
+        "networking/wifi/home/login" = {};
+        "networking/wifi/home/password" = {};
+      };
+
+      templates."wireless.env" = {
+        content = ''
+          HOME_WIFI_SSID=${config.sops.placeholder."networking/wifi/home/login"}
+          HOME_WIFI_PASSWORD=${config.sops.placeholder."networking/wifi/home/password"}
+        '';
+      };
+    };
+
     # Configure network proxy if necessary
     # networking.proxy.default = "http://user:password@proxy:port/";
     # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
     # Enable networking
-    networking.networkmanager.enable = true;
+    networking.networkmanager = {
+      enable = true;
+      ensureProfiles = {
+        environmentFiles = [ config.sops.templates."wireless.env".path ];
+        profiles =  {
+          home-wifi = {
+            connection.id = "home-wifi";
+            connection.type = "wifi";
+            wifi.ssid = "$HOME_WIFI_SSID";
+            wifi-security = {
+              auth-alg = "open";
+              key-mgmt = "wpa-psk";
+              psk = "$HOME_WIFI_PASSWORD";
+            };
+          };
+        };
+      };
+    };
 
     # Open ports in the firewall.
     # networking.firewall.allowedTCPPorts = [ ... ];
