a1e74f9550a4928b299d8cd7698e3e44e5fd12c3
NickRI <nicktt2008@yandex.ru>
2025-02-08 19:22:14 +0100

    Delete unnecessary fonts

--- nas-server/home/default.nix
+++ nas-server/home/default.nix
@@ -22,9 +22,6 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs.unstable; [
-    nerd-fonts.meslo-lg
-    nerd-fonts.dejavu-sans-mono
-
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
