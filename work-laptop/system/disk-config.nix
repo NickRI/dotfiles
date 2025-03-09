df374c2e7445645bd1be50ebbb3120776049b7e7
NickRI <nicktt2008@yandex.ru>
2025-03-09 02:41:35 +0100

    Enable disko

--- work-laptop/system/disk-config.nix
+++ work-laptop/system/disk-config.nix
@@ -1,54 +1,50 @@
-{
-  config,
-  disko,
-  lib,
-  ...
-}:
+{ ... }:
 
 {
-  #  disko.devices = {
-  #    disk = {
-  #      sda = {
-  #        device = lib.mkDefault "/dev/sda";
-  #        type = "disk";
-  #        content = {
-  #          type = "gpt";
-  #          partitions = {
-  #            ESP = {
-  #              type = "EF00";
-  #              size = "550M";
-  #              content = {
-  #                type = "filesystem";
-  #                format = "vfat";
-  #                mountpoint = "/boot";
-  #              };
-  #            };
-  #            root = {
-  #              size = "100%";
-  #              content = {
-  #                type = "filesystem";
-  #                format = "ext4";
-  #                mountpoint = "/";
-  #              };
-  #            };
-  #          };
-  #        };
-  #      };
-  #    };
-  #  };
-
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/cc468b23-5e57-4006-ae4b-25bec0fccfca";
-    fsType = "ext4";
-    options = [ "noatime" ];
+  disko.devices = {
+    disk = {
+      nvme0n1 = {
+        device = "/dev/nvme0n1";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "550M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            windows = {
+              size = "160GB";
+              content = {
+                type = "filesystem";
+                format = "ntfs";
+              };
+            };
+            swap = {
+              size = "34GB";
+              content = {
+                type = "swap";
+                discardPolicy = "both";
+                resumeDevice = true; # resume from hibernation from this device
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
   };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/558C-E1E4";
-    fsType = "vfat";
-  };
-
-  swapDevices = [
-    { device = "/dev/disk/by-uuid/7016fe1f-f046-4221-a404-10ffee6615e0"; }
-  ];
 }
