5210a429bc88ced2331f38c712dafdffcb5820a1
NickRI <nicktt2008@yandex.ru>
2025-03-15 06:28:48 +0100

    Update email profile and extensions

--- work-laptop/home/modules/email.nix
+++ work-laptop/home/modules/email.nix
@@ -1,4 +1,9 @@
-{ config, sops-secrets, ... }:
+{
+  config,
+  pkgs,
+  sops-secrets,
+  ...
+}:
 let
   accounts = builtins.fromJSON (builtins.readFile "${toString sops-secrets}/accounts.json");
 
@@ -13,23 +18,35 @@ let
 in
 {
   config = {
-    sops.secrets = {
-      "accounts-passwords/primary" = { };
-      "accounts-passwords/secondary" = { };
-      "accounts-passwords/business" = { };
-      "accounts-passwords/work" = { };
-    };
-
     programs = {
       thunderbird = {
         enable = true;
+        settings = {
+          "font.language.group" = "x-cyrillic";
+          "intl.accept_languages" = "ru-RU, ru, en-US, en";
+          "intl.locale.requested" = "ru,en-US";
+        };
+        package = pkgs.thunderbird.override {
+          extraPolicies.ExtensionSettings = {
+            "langpack-ru@thunderbird.mozilla.org" = {
+              installation_mode = "normal_installed";
+              install_url = "https://addons.thunderbird.net/thunderbird/downloads/file/1037160/russian_ru_language_pack-137.0.20250311.191724-tb.xpi?src=";
+            };
+            "mas@aandrzej.com" = {
+              installation_mode = "normal_installed";
+              install_url = "https://addons.thunderbird.net/thunderbird/downloads/file/1030065/minimize_on_startup-1.1-tb.xpi?src=";
+            };
+            "minimizeonclose@rsjtdrjgfuzkfg.com" = {
+              installation_mode = "normal_installed";
+              install_url = "https://addons.thunderbird.net/thunderbird/downloads/file/1030066/minimize_on_close-2.0.1.4-tb.xpi?src=";
+            };
+          };
+        };
         profiles = {
           default = {
             isDefault = true;
             settings = {
-              "font.language.group" = "x-cyrillic";
-              "intl.accept_languages" = "ru-RU, ru, en-US, en";
-              "intl.locale.requested" = "ru,en-US";
+              "extensions.autoDisableScopes" = 0;
             };
           };
         };
@@ -43,43 +60,25 @@ in
           thunderbird = thunderbird-setup;
           address = accounts.primary;
           realName = "Nick Noname";
-          passwordCommand = [ "cat ${config.sops.secrets."accounts-passwords/primary".path}" ];
           flavor = "gmail.com";
         };
         "${accounts.secondary}" = {
           thunderbird = thunderbird-setup;
           address = accounts.secondary;
           realName = "Nick Noname";
-          passwordCommand = [ "cat ${config.sops.secrets."accounts-passwords/secondary".path}" ];
           flavor = "yandex.com";
         };
         "${accounts.business}" = {
           thunderbird = thunderbird-setup;
           address = accounts.business;
           realName = "ITWorks";
-          passwordCommand = [ "cat ${config.sops.secrets."accounts-passwords/business".path}" ];
           flavor = "gmail.com";
         };
         "${accounts.work}" = {
           thunderbird = thunderbird-setup;
           address = accounts.work;
           realName = "Nick";
-          passwordCommand = [ "cat ${config.sops.secrets."accounts-passwords/work".path}" ];
           flavor = "gmail.com";
-        };
-      };
-
-      contact = {
-        basePath = ".accounts/contacts";
-        accounts = {
-          primary = {
-            name = accounts.primary;
-            remote = {
-              userName = accounts.primary;
-              passwordCommand = [ "cat ${config.sops.secrets."accounts-passwords/primary".path}" ];
-              type = "google_contacts";
-            };
-          };
         };
       };
     };
