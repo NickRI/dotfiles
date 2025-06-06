756ea28d621454eb57c769fad062382e7cb49d52
NickRI <nicktt2008@yandex.ru>
2025-06-07 01:26:00 +0200

    Use latest kernel 6.15.1

--- shared/platforms/framework-13-amd-ai-300-series.nix
+++ shared/platforms/framework-13-amd-ai-300-series.nix
@@ -4,20 +4,7 @@
   nixos-hardware,
   ...
 }:
-let
-  pinnedKernel = pkgs.linuxPackagesFor (
-    pkgs.linux_6_14.override {
-      argsOverride = rec {
-        version = "6.14.6";
-        modDirVersion = version;
-        src = pkgs.fetchurl {
-          url = "mirror://kernel/linux/kernel/v6.x/linux-${version}.tar.xz";
-          sha256 = "sha256-IYF/GZjiIw+B9+T2Bfpv3LBA4U+ifZnCfdsWznSXl6k=";
-        };
-      };
-    }
-  );
-in
+
 {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
@@ -31,5 +18,5 @@ in
     "usbhid"
   ];
 
-  boot.kernelPackages = pinnedKernel; # TODO: Check newer versions of kernels for hibernation
+  boot.kernelPackages = pkgs.linuxPackages_latest; # TODO: Check newer versions of kernels for hibernation
 }
