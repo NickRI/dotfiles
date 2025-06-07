7cd30f64c06fade78f5995f2a3b7ef1eaad2b857
NickRI <nicktt2008@yandex.ru>
2025-06-07 21:54:45 +0200

    Complete install.nix

--- shared/system/install.nix
+++ shared/system/install.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }:
 let
+  wormhole-drop = import ../tools/wormhole-drop {
+    inherit pkgs;
+  };
+
   install = pkgs.writeShellScriptBin "install-dotfiles" ''
     #!/usr/bin/env bash
 
@@ -10,15 +14,20 @@ let
       "$@"
     }
 
-    if [ ! -f $HOME/.ssh/id_ed25519.pub ]; then
-      ssh-keygen -t ed25519 -C "nixos@hostname" -f $HOME/.ssh/id_ed25519 -N ""
+    logrun nmtui connect
+
+    if [ ! -f "$HOME/.ssh/id_ed25519" ] || [ ! -f "$HOME/.ssh/nix-secrets" ]; then
+      read -p "Enter wormhole-drop listen address (leave empty for 0.0.0.0:80): " listen
+      if [ -z "$listen" ]; then
+        listen="0.0.0.0:80"
+      fi
+
+      logrun ${wormhole-drop}/bin/wormhole-drop -listen $listen -file1=$HOME/.ssh/id_ed25519 -file2=$HOME/.ssh/nix-secrets
+      logrun chmod 600 ~/.ssh/id_ed25519 ~/.ssh/nix-secrets
     fi
 
-    cat $HOME/.ssh/id_ed25519.pub
-    read -p "You need to add this key to the https://github.com/settings/ssh/new, press enter when ready to go"
-
     if [ ! -d $HOME/dotfiles ]; then
-     logrun git clone git@github.com:nickRI/dotfiles
+      logrun git clone git@github.com:nickRI/dotfiles
     fi
 
     if [ ! -d $HOME/dotfiles/nix-secrets ]; then
@@ -27,14 +36,11 @@ let
 
     echo "Rewrite flake secret path"
 
-    logrun sed -i "s|git+ssh://git@github.com/NickRI/nix-secrets.git?ref=main&shallow=1|path:$HOME/dotfiles/nix-secrets|" $HOME/dotfiles/flake.nix
+    logrun nix flake lock $HOME/dotfiles --update-input sops-secrets --override-input sops-secrets path:$HOME/dotfiles/nix-secrets
 
     configuration=$(nix flake show --json $HOME/dotfiles | nix run "nixpkgs#jq" -- -r '.nixosConfigurations | keys[]' | nix run "nixpkgs#fzf" -- --header="Please select your configuration:" --prompt="configuration: ")
 
-    read -p "Enter disko mode could be either destroy, format, mount, unmount, format,mount or destroy,format,mount (leave empty for format,mount): " disko_mode
-    if [ -z "$disko_mode" ]; then
-      disko_mode="format,mount"
-    fi
+    disko_mode=$(echo "format,mount;destroy,format,mount;destroy;format;mount;unmount" | tr ';' '\n' | fzf --header="Please select your format mode for disko:" --prompt="Format mode: ")
 
     read -p "Enter optional substituters (leave empty for https://ncps.nas.firefly.red): " substituters
     if [ -n "$substituters" ]; then
@@ -47,7 +53,20 @@ let
 
     logrun lsblk
 
+    echo "Process age key"
+
+    echo "Your key from $HOME/.ssh/nix-secrets: $(ssh-keygen -f $HOME/.ssh/nix-secrets -y | nix run nixpkgs#ssh-to-age)"
+    echo "Available sops keys $HOME/.ssh/nix-secrets: $(cat $HOME/dotfiles/nix-secrets/.sops.yaml)"
+
+    sopsKeyFile=$(nix eval --raw $HOME/dotfiles#nixosConfigurations.work-laptop.config.sops.age.keyFile)
+
+    logrun sudo nix run nixpkgs#ssh-to-age  -- -i $HOME/.ssh/nix-secrets -private-key -o /mnt$sopsKeyFile
+
     logrun sudo nixos-install $nix_options --flake "$HOME/dotfiles#$configuration" --root /mnt --no-root-password
+
+    echo "Clear master age key"
+
+    logrus sudo rm /mnt$sopsKeyFile
   '';
 in
 {
