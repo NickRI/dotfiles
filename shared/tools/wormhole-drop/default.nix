71923cfb38e5d3e9aee86bf44bcbacfa87680113
NickRI <nicktt2008@yandex.ru>
2025-06-08 00:45:16 +0200

    Use toolchain and prev stable go version Fix build name

--- shared/tools/wormhole-drop/default.nix
+++ shared/tools/wormhole-drop/default.nix
@@ -1,33 +1,12 @@
-{
-  pkgs ? import <nixpkgs> { },
-}:
+{ pkgs, ... }:
 
-pkgs.stdenv.mkDerivation {
+pkgs.buildGoModule {
   pname = "wormhole-drop";
   version = "0.1.0";
 
   src = ./.;
 
-  buildInputs = [
-    pkgs.go
-    pkgs.gcc
-  ];
-
-  buildPhase = ''
-    export GOCACHE=$TMPDIR/go-build-cache
-    export GOMODCACHE=$TMPDIR/go-mod-cache
-    mkdir -p $GOCACHE
-    mkdir -p $GOMODCACHE
-
-    cd $src
-
-    go build -o $out/bin/wormhole-drop .
-  '';
-
-  installPhase = ''
-    # бинарник уже на месте
-    :
-  '';
+  vendorHash = null;
 
   meta = with pkgs.lib; {
     description = "Go wormhole drop application";
--- shared/tools/wormhole-drop/go.mod
+++ shared/tools/wormhole-drop/go.mod
@@ -1,5 +1,7 @@
-module install
+module wormhole-drop
 
-go 1.24.3
+go 1.23.8
+
+toolchain go1.24.3
 
 require golang.org/x/crypto v0.39.0
