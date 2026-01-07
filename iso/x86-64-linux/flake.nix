549e5a83615c06a353ce2aa0b2da24792d2c17f1
NickRI <nicktt2008@yandex.ru>
2026-01-07 01:51:18 +0100

    Update version

--- iso/x86-64-linux/flake.nix
+++ iso/x86-64-linux/flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS drop-in image for first boot";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
   };
 
   outputs =
