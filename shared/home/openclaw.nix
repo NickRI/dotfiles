78ff1b033af2e0b3c6d625965a2e4fa954d31679
NickRI <nicktt2008@yandex.ru>
2026-03-06 13:44:44 +0100

    Multiple changes

--- shared/home/openclaw.nix
+++ shared/home/openclaw.nix
@@ -8,9 +8,20 @@
     {
       "bots/telegram-token" = openclaw;
       "gateway-token" = openclaw;
-      "models-keys" = openclaw;
+      "models-keys/openai" = openclaw;
+      "models-keys/deepseek" = openclaw;
       "agents/gitea-keys" = openclaw;
     };
+
+  sops.templates = {
+    "openclaw-env" = {
+      path = "${config.home.homeDirectory}/.openclaw/.env";
+      content = ''
+        OPENAI_API_KEY=${config.sops.placeholder."models-keys/openai"}
+        DEEPSEEK_API_KEY=${config.sops.placeholder."models-keys/deepseek"}
+      '';
+    };
+  };
 
   home.file = {
     ".openclaw/openclaw.json".force = true;
@@ -27,7 +38,6 @@
 
   systemd.user.services.openclaw-gateway = {
     Service.EnvironmentFile = [
-      config.sops.secrets."models-keys".path
       config.sops.secrets."agents/gitea-keys".path
     ];
     Install.WantedBy = [ "default.target" ];
@@ -60,19 +70,56 @@
         };
       };
 
-      #      models = {
-      #        providers = {
-      #          openai = {
-      #            enabled = true;
-      #          };
-      #        };
-      #      };
+      models = {
+        mode = "merge";
+        providers = {
+          deepseek = {
+            baseUrl = "https://api.deepseek.com";
+            apiKey = {
+              source = "env";
+              provider = "default";
+              id = "DEEPSEEK_API_KEY";
+            };
+            api = "openai-completions";
+            models = [
+              {
+                id = "deepseek-chat";
+                name = "DeepSeek Chat (V3.2)";
+                reasoning = false;
+                input = [ "text" ];
+                cost = {
+                  input = 0.00000028;
+                  output = 0.00000042;
+                  cacheRead = 0.000000028;
+                  cacheWrite = 0.00000028;
+                };
+                contextWindow = 128000;
+                maxTokens = 8192;
+              }
+              {
+                id = "deepseek-reasoner";
+                name = "DeepSeek Reasoner (V3.2)";
+                reasoning = true;
+                input = [ "text" ];
+                cost = {
+                  input = 0.00000028;
+                  output = 0.00000042;
+                  cacheRead = 0.000000028;
+                  cacheWrite = 0.00000028;
+                };
+                contextWindow = 128000;
+                maxTokens = 65536;
+              }
+            ];
+          };
+        };
+      };
 
       agents = {
         defaults = {
           model = {
-            primary = "openai/gpt-5.1-codex";
-            fallbacks = [ "openai/gpt-5.1-codex" ];
+            primary = "deepseek/deepseek-chat";
+            fallbacks = [ "deepseek/deepseek-reasoner" ];
           };
         };
       };
