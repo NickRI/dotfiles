6af4ec23742192b1ad0aa433d2fffc8e7fe7aeb0
NickRI <nicktt2008@yandex.ru>
2026-04-15 13:50:02 +0200

    Ollama for local development

--- work-laptop/system/modules/ai.nix
+++ work-laptop/system/modules/ai.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
   hardware.graphics = {
@@ -8,9 +8,16 @@
     ];
   };
 
+  networking.firewall.trustedInterfaces = [
+    "docker0"
+    "ascend-br"
+  ];
+
   services = {
     ollama = {
       enable = true;
+      host = "0.0.0.0";
+      openFirewall = true;
       package = pkgs.unstable.ollama-rocm;
       environmentVariables = {
         HSA_OVERRIDE_GFX_VERSION = "11.0.0";
