f9ca67856fc77a9440a21dfc659ee31b2b130bfd
NickRI <nicktt2008@yandex.ru>
2026-06-28 03:01:37 +0300

    Delete config

--- work-laptop/home/modules/vpn.nix
+++ work-laptop/home/modules/vpn.nix
@@ -1,13 +1,11 @@
 { config, lib, ... }:
 
 {
-  config = {
-    services.flatpak.packages = lib.mkIf (config.services.flatpak.enable) [ "com.surfshark.Surfshark" ];
+  services.flatpak.packages = lib.mkIf (config.services.flatpak.enable) [ "com.surfshark.Surfshark" ];
 
-    programs.chromium = lib.mkIf (config.programs.chromium.enable) {
-      extensions = [
-        { id = "ailoabdmgclmfmhdagmlohpjlbpffblp"; } # Surfshark VPN
-      ];
-    };
+  programs.chromium = lib.mkIf (config.programs.chromium.enable) {
+    extensions = [
+      { id = "ailoabdmgclmfmhdagmlohpjlbpffblp"; } # Surfshark VPN
+    ];
   };
 }
